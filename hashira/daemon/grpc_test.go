d0226104897ce557c97401a67b5c0568455bb4df
pankona <yosuke.akatsuka@gmail.com>
2018-07-11 10:25:44 +0900

    add unittest for endpoint Retrieve

--- hashira/daemon/grpc_test.go
+++ hashira/daemon/grpc_test.go
@@ -18,6 +18,16 @@ func (m *mockDB) Save(id string, value []byte) error {
 	return nil
 }
 
+func (m *mockDB) ForEach(f func(k, v []byte) error) error {
+	for k, v := range m.data {
+		err := f([]byte(k), v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func TestEndPointCreate(t *testing.T) {
 	d := &Daemon{
 		DB: &mockDB{data: make(map[string][]byte)},
@@ -55,4 +65,47 @@ func TestEndPointCreate(t *testing.T) {
 }
 
 func TestEndPointRetrieve(t *testing.T) {
+	d := &Daemon{
+		DB: &mockDB{data: make(map[string][]byte)},
+	}
+
+	tcs := []struct {
+		inName    string
+		inPlace   service.Place
+		wantName  string
+		wantPlace service.Place
+	}{
+		{
+			inName: "test", inPlace: service.Place_BACKLOG,
+			wantName: "test", wantPlace: service.Place_BACKLOG,
+		},
+	}
+	for _, tc := range tcs {
+		cc := &service.CommandCreate{
+			Name:  tc.inName,
+			Place: tc.inPlace,
+		}
+		result, err := d.Create(context.Background(), cc)
+		if err != nil {
+			t.Fatalf("Create returned unexpected error: %s", err.Error())
+		}
+		if result.GetTask().GetName() != tc.wantName {
+			t.Errorf("unexpected result. [got] %v [want] %v", result.GetTask().GetName(), tc.wantName)
+		}
+
+		if result.GetTask().GetPlace() != tc.wantPlace {
+			t.Errorf("unexpected result. [got] %v [want] %v", result.GetTask().GetPlace(), tc.wantPlace)
+		}
+	}
+
+	rc := &service.CommandRetrieve{}
+	result, err := d.Retrieve(context.Background(), rc)
+	if err != nil {
+		t.Fatalf("Retrieve returned unexpected error: %s", err.Error())
+	}
+
+	tasks := result.GetTasks()
+	if len(tasks) != len(tcs) {
+		t.Errorf("unexpected result. [got] %d [want] %d", len(tasks), len(tcs))
+	}
 }
