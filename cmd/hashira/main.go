79b7047ab2c0a7da496ac4c80e0e295225266bd4
pankona <yosuke.akatsuka@gmail.com>
2018-08-12 00:42:28 +0900

    add list subcommand

--- cmd/hashira/main.go
+++ cmd/hashira/main.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
 	"strconv"
 
 	"github.com/pankona/hashira/hashira/client"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
@@ -29,19 +28,23 @@ func main() {
 		})
 	)
 
+	listCmd := kingpin.Command("list", "show list of tasks")
+	var (
+		_ = listCmd.Action(func(pc *kingpin.ParseContext) error {
+			tasks, err := c.Retrieve(ctx)
+			if err != nil {
+				return errors.New("failed to create a new task: " + err.Error())
+			}
+			for _, v := range tasks {
+				id, err := strconv.Atoi(v.Id)
+				if err != nil {
+					continue
+				}
+				fmt.Printf("[%04d]\t%s\t%v\t%v\n", id, v.Name, v.Place, v.IsDeleted)
+			}
+			return nil
+		})
+	)
+
 	kingpin.Parse()
-
-	tasks, err := c.Retrieve(ctx)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	for _, v := range tasks {
-		id, err := strconv.Atoi(v.Id)
-		if err != nil {
-			continue
-		}
-		fmt.Printf("[%04d]\t%s\t%v\t%v\n", id, v.Name, v.Place, v.IsDeleted)
-	}
 }
