6496331de58325899688220661fc5f3f97d1fb49
pankona <yosuke.akatsuka@gmail.com>
2018-08-12 00:58:25 +0900

    move addXXXCmd implementation to commands.go

--- cmd/hashira/commands.go
+++ cmd/hashira/commands.go
@@ -8,7 +8,30 @@ import (
 	"strconv"
 
 	"github.com/pankona/hashira/hashira/client"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
+
+func addNewCmd(ctx context.Context, c *client.Client) {
+	newCmd := kingpin.Command(
+		"new",
+		"add new task with specified task name")
+	name := newCmd.Arg(
+		"name",
+		"name of task which is newly added").
+		Required().String()
+	_ = newCmd.Action(func(pc *kingpin.ParseContext) error {
+		return create(ctx, c, *name)
+	})
+}
+
+func addListCmd(ctx context.Context, c *client.Client) {
+	listCmd := kingpin.Command(
+		"list",
+		"show list of tasks")
+	_ = listCmd.Action(func(pc *kingpin.ParseContext) error {
+		return list(ctx, c)
+	})
+}
 
 func create(ctx context.Context, c *client.Client, name string) error {
 	err := c.Create(ctx, name)
--- cmd/hashira/main.go
+++ cmd/hashira/main.go
@@ -18,25 +18,3 @@ func main() {
 
 	kingpin.Parse()
 }
-
-func addNewCmd(ctx context.Context, c *client.Client) {
-	newCmd := kingpin.Command(
-		"new",
-		"add new task with specified task name")
-	name := newCmd.Arg(
-		"name",
-		"name of task which is newly added").
-		Required().String()
-	_ = newCmd.Action(func(pc *kingpin.ParseContext) error {
-		return create(ctx, c, *name)
-	})
-}
-
-func addListCmd(ctx context.Context, c *client.Client) {
-	listCmd := kingpin.Command(
-		"list",
-		"show list of tasks")
-	_ = listCmd.Action(func(pc *kingpin.ParseContext) error {
-		return list(ctx, c)
-	})
-}
