cd6b8d1b1569328e9daaec214c9898819d3554ff
pankona <yosuke.akatsuka@gmail.com>
2018-09-03 00:19:55 +0900

    let render function DRY

--- cmd/hashira-cui/pane.go
+++ cmd/hashira-cui/pane.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/jroimartin/gocui"
 	"github.com/pankona/hashira/service"
@@ -19,40 +20,28 @@ type Pane struct {
 func (p *Pane) Layout(g *gocui.Gui, selectedIndex int) error {
 	maxX, maxY := g.Size()
 	v, err := g.SetView(p.name, maxX/4*p.index, 1, maxX/4*p.index+maxX/4-1, maxY-1)
+
 	if err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
 		v.Title = p.name
-
-		var itemNum int
-		// render tasks for this pane
-		for _, task := range p.tasks {
-			if task.Place == p.place && !task.IsDeleted {
-				var err error
-				if itemNum == selectedIndex {
-					_, err = fmt.Fprintf(v, "\033[3%d;%dm%s\033[0m\n", 7, 4, task.Name)
-				} else {
-					_, err = fmt.Fprintf(v, "%s\n", task.Name)
-				}
-				if err != nil {
-					return err
-				}
-				itemNum++
-			}
-		}
-		return nil
 	}
 
 	v.Clear()
+	return renderTasks(v, p.place, p.tasks, selectedIndex)
+}
+
+func renderTasks(w io.Writer, place service.Place, tasks []*service.Task, selectedIndex int) error {
 	var itemNum int
-	for _, task := range p.tasks {
-		if task.Place == p.place && !task.IsDeleted {
+	// render tasks for this pane
+	for _, task := range tasks {
+		if task.Place == place && !task.IsDeleted {
 			var err error
 			if itemNum == selectedIndex {
-				_, err = fmt.Fprintf(v, "\033[3%d;%dm%s\033[0m\n", 7, 4, task.Name)
+				_, err = fmt.Fprintf(w, "\033[3%d;%dm%s\033[0m\n", 7, 4, task.Name)
 			} else {
-				_, err = fmt.Fprintf(v, "%s\n", task.Name)
+				_, err = fmt.Fprintf(w, "%s\n", task.Name)
 			}
 			if err != nil {
 				return err
