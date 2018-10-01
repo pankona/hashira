671c12ba4cccc315e10fbb1fec1d84b62376350f
pankona <yosuke.akatsuka@gmail.com>
2018-10-01 16:37:29 +0900

    add missing comments on exported functions on pubsub.go

--- cmd/hashira-cui/pubsub.go
+++ cmd/hashira-cui/pubsub.go
@@ -2,18 +2,22 @@ package main
 
 import "sync"
 
+// Subscriber is an interface of PubSub subscriber
 type Subscriber interface {
 	OnEvent(event string, data ...interface{})
 }
 
+// Publisher is an interface of PubSub publisher
 type Publisher interface {
 	Publish(event string, data ...interface{})
 }
 
+// PubSub is a struct for pubsub message passing
 type PubSub struct {
 	ss sync.Map
 }
 
+// Publish publishes specified event and data to subscribers
 func (ps *PubSub) Publish(event string, data ...interface{}) {
 	ps.ss.Range(func(k, v interface{}) bool {
 		s := v.(Subscriber)
@@ -22,6 +26,7 @@ func (ps *PubSub) Publish(event string, data ...interface{}) {
 	})
 }
 
+// Subscribe registers subscriber to PubSub
 func (ps *PubSub) Subscribe(id string, s Subscriber) {
 	ps.ss.Store(id, s)
 }
