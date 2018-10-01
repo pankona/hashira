aeabaf953aa18427d66e20822fe1244834a8fa50
pankona <yosuke.akatsuka@gmail.com>
2018-10-01 16:28:22 +0900

    add missing comments on exported functions on model.go

--- cmd/hashira-cui/model.go
+++ cmd/hashira-cui/model.go
@@ -7,22 +7,27 @@ import (
 	"github.com/pankona/hashira/service"
 )
 
+// Model represents model of hashira's mvc
 type Model struct {
 	hashirac *hashirac.Client
 }
 
+// SetHashiraClient sets hashira client
 func (m *Model) SetHashiraClient(cli *hashirac.Client) {
 	m.hashirac = cli
 }
 
+// List retrieves task list using hashira client
 func (m *Model) List(ctx context.Context) ([]*service.Task, error) {
 	return m.hashirac.Retrieve(ctx)
 }
 
+// RetrievePriority retrieves priorities using hashira client
 func (m *Model) RetrievePriority(ctx context.Context) ([]*service.Priority, error) {
 	return m.hashirac.RetrievePriority(ctx)
 }
 
+// UpdatePriority updates priorities using hashira client
 func (m *Model) UpdatePriority(ctx context.Context, priorities []*service.Priority) ([]*service.Priority, error) {
 	return m.hashirac.UpdatePriority(ctx, priorities)
 }
