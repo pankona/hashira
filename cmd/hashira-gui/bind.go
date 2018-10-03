fb883c8a7fe29e1eb0cba61dafb9cb80b5e2d9f2
pankona <yosuke.akatsuka@gmail.com>
2018-10-03 12:59:05 +0900

    [#31] add missing comments on exported functions

--- cmd/hashira-gui/bind.go
+++ cmd/hashira-gui/bind.go
@@ -2,10 +2,12 @@
 
 package main
 
+// Asset is just a mock function
 func Asset(name string) ([]byte, error) {
 	return nil, nil
 }
 
+// RestoreAssets is just mock function
 func RestoreAssets(dir, name string) error {
 	return nil
 }
