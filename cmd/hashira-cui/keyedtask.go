ad3491fa19e4a70b298f265aa9e6df3214ec425a
pankona <yosuke.akatsuka@gmail.com>
2018-10-12 14:38:03 +0900

    add missing comments on exported struct and function

--- cmd/hashira-cui/keyedtask.go
+++ cmd/hashira-cui/keyedtask.go
@@ -2,8 +2,11 @@ package main
 
 import "github.com/pankona/hashira/service"
 
+// KeyedTask is service.Task with Key() function.
 type KeyedTask service.Task
 
+// Key returns task's ID.
+// This function is for satisfying orderedmap.Keyer interface.
 func (kt *KeyedTask) Key() string {
 	return kt.Id
 }
