a99421dfd20c156cda1a6cbdcf5325241e615524
pankona <yosuke.akatsuka@gmail.com>
2019-10-10 00:27:29 +0900

    add json tag on User struct

--- user/user.go
+++ user/user.go
@@ -2,8 +2,8 @@ package user
 
 // User is entity to represent a user
 type User struct {
-	ID        string
-	Name      string
-	TwitterID string
-	GoogleID  string
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	GoogleID  string `json:"google_id"`
+	TwitterID string `json:"twitter_id"`
 }
