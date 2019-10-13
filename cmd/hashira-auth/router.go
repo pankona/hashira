467ee252d0daf8d1beb565163b3cf1955847d84c
pankona <yosuke.akatsuka@gmail.com>
2019-10-13 13:17:07 +0900

    add router.go for routing

---
+++ cmd/hashira-auth/router.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/pankona/hashira/auth/accesstoken"
+	"github.com/pankona/hashira/auth/google"
+	"github.com/pankona/hashira/auth/me"
+	"github.com/pankona/hashira/auth/twitter"
+	"github.com/pankona/hashira/kvstore"
+)
+
+type router struct {
+	mux            *http.ServeMux
+	kvs            kvstore.KVStore
+	servingBaseURL string
+}
+
+func (r *router) route() {
+	r.handleMe("/api/v1/me")
+	r.handleAccessToken("/api/v1/accesstoken")
+	r.handleGoogleOAuth(r.servingBaseURL, "/auth/google")
+	r.handleTwitterOAuth(r.servingBaseURL, "/auth/twitter")
+}
+
+func (r *router) handleMe(path string) {
+	r.mux.Handle(
+		path,
+		http.StripPrefix(path, me.New(r.kvs)))
+}
+
+func (r *router) handleAccessToken(path string) {
+	r.mux.Handle(
+		path,
+		http.StripPrefix(path, accesstoken.New()))
+}
+
+func (r *router) handleGoogleOAuth(servingBaseURL, path string) {
+	g := google.New(
+		os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
+		os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"),
+		servingBaseURL+path+"/callback",
+		r.kvs)
+
+	r.mux.Handle(
+		path,
+		http.StripPrefix(path, g))
+
+	r.mux.Handle(
+		path+"/callback",
+		http.StripPrefix(path, g))
+}
+
+func (r *router) handleTwitterOAuth(servingBaseURL, path string) {
+	t := twitter.New(
+		os.Getenv("TWITTER_API_TOKEN"),
+		os.Getenv("TWITTER_API_SECRET"),
+		os.Getenv("TWITTER_API_ACCESS_TOKEN"),
+		os.Getenv("TWITTER_API_ACCESS_TOKEN_SECRET"),
+		servingBaseURL+path+"/callback",
+		r.kvs)
+
+	r.mux.Handle(
+		path,
+		http.StripPrefix(path, t))
+
+	r.mux.Handle(
+		path+"/callback",
+		http.StripPrefix(path, t))
+}
