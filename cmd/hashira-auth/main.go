ff90ca56b7d0eb22a230d45d8d76195564aa09f1
pankona <yosuke.akatsuka@gmail.com>
2019-10-18 22:02:02 +0900

    re-organize routes

--- cmd/hashira-auth/main.go
+++ cmd/hashira-auth/main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/pankona/hashira/auth/accesstoken"
+	"github.com/pankona/hashira/auth/google"
+	"github.com/pankona/hashira/auth/me"
+	"github.com/pankona/hashira/auth/twitter"
 	"github.com/pankona/hashira/store"
 )
 
@@ -25,14 +29,61 @@ func main() {
 	log.Printf("servingBaseURL: %v", servingBaseURL)
 
 	ms := store.NewMemStore()
+
+	googleOAuthHandler := google.New(
+		os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
+		os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"),
+		servingBaseURL+"/auth/google/callback",
+		ms)
+
+	twitterOAuthHandler := twitter.New(
+		os.Getenv("TWITTER_API_TOKEN"),
+		os.Getenv("TWITTER_API_SECRET"),
+		os.Getenv("TWITTER_API_ACCESS_TOKEN"),
+		os.Getenv("TWITTER_API_ACCESS_TOKEN_SECRET"),
+		servingBaseURL+"/auth/twitter/callback",
+		ms)
+
 	r := router{
-		mux:            http.DefaultServeMux,
-		meStore:        ms,
-		googleStore:    ms,
-		twitterStore:   ms,
-		servingBaseURL: servingBaseURL,
+		mux: http.DefaultServeMux,
+		routes: map[http.Handler]*routes{
+			me.New(ms): {
+				"/api/v1/me",
+				[]string{
+					"",
+				},
+			},
+			accesstoken.New(): {
+				"/api/v1/accesstoken",
+				[]string{
+					"",
+				},
+			},
+			googleOAuthHandler: {
+				"/auth/google",
+				[]string{
+					"",
+					"/callback",
+				},
+			},
+			twitterOAuthHandler: {
+				"/auth/twitter",
+				[]string{
+					"",
+					"/callback",
+				},
+			},
+		},
 	}
+
 	r.route()
+	//r.mux.Handle("/", &root{})
 
 	log.Fatal(http.ListenAndServe(":"+port, r.mux))
+}
+
+type root struct{}
+
+func (ro *root) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("r.URL.Path: %v", r.URL.Path)
 }
--- cmd/hashira-auth/router.go
+++ cmd/hashira-auth/router.go
@@ -1,61 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"os"
-
-	"github.com/pankona/hashira/auth/accesstoken"
-	"github.com/pankona/hashira/auth/google"
-	"github.com/pankona/hashira/auth/me"
-	"github.com/pankona/hashira/auth/twitter"
 )
 
 type router struct {
-	mux            *http.ServeMux
-	servingBaseURL string
+	mux    *http.ServeMux
+	routes map[http.Handler]*routes
+}
 
-	meStore      me.UserStore
-	googleStore  google.UserStore
-	twitterStore twitter.UserStore
+type routes struct {
+	path      string
+	subroutes []string
 }
 
 func (r *router) route() {
-	r.handleMe("/api/v1/me")
-	r.handleAccessToken("/api/v1/accesstoken")
-	r.handleGoogleOAuth(r.servingBaseURL, "/auth/google")
-	r.handleTwitterOAuth(r.servingBaseURL, "/auth/twitter")
-}
-
-func (r *router) handleMe(path string) {
-	r.mux.Handle(
-		path,
-		http.StripPrefix(path, me.New(r.meStore)))
-}
-
-func (r *router) handleAccessToken(path string) {
-	r.mux.Handle(
-		path,
-		http.StripPrefix(path, accesstoken.New()))
-}
-
-func (r *router) handleGoogleOAuth(servingBaseURL, path string) {
-	g := google.New(
-		os.Getenv("GOOGLE_OAUTH2_CLIENT_ID"),
-		os.Getenv("GOOGLE_OAUTH2_CLIENT_SECRET"),
-		servingBaseURL+path+"/callback",
-		r.googleStore)
-	r.mux.Handle(path, http.StripPrefix(path, g))
-	r.mux.Handle(path+"/callback", http.StripPrefix(path, g))
-}
-
-func (r *router) handleTwitterOAuth(servingBaseURL, path string) {
-	t := twitter.New(
-		os.Getenv("TWITTER_API_TOKEN"),
-		os.Getenv("TWITTER_API_SECRET"),
-		os.Getenv("TWITTER_API_ACCESS_TOKEN"),
-		os.Getenv("TWITTER_API_ACCESS_TOKEN_SECRET"),
-		servingBaseURL+path+"/callback",
-		r.twitterStore)
-	r.mux.Handle(path, http.StripPrefix(path, t))
-	r.mux.Handle(path+"/callback", http.StripPrefix(path, t))
+	for handler, routes := range r.routes {
+		for _, subroute := range routes.subroutes {
+			r.mux.Handle(fmt.Sprintf("%s%s", routes.path, subroute),
+				http.StripPrefix(routes.path, handler))
+		}
+	}
 }
