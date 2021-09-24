fe567fe6cbf6a919175f35d22ffe555f0f75c1d7
pankona <yosuke.akatsuka@gmail.com>
2021-09-24 22:39:52 +0900

    add a function to upload task and priority

--- hashira-web/functions/hashira/hashira.go
+++ hashira-web/functions/hashira/hashira.go
@@ -2,11 +2,12 @@ package hashira
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"regexp"
-
-	"github.com/pankona/hashira/hashira-web/functions/hashira/store"
 )
 
 var bearerRegex = regexp.MustCompile("(?i)^bearer (.*)$")
@@ -15,35 +16,33 @@ type AccessTokenStore interface {
 	FindUidByAccessToken(ctx context.Context, accesstoken string) (string, error)
 }
 
-type Hashira struct {
-	AccessTokenStore AccessTokenStore
+type TaskAndPriorityStore interface {
+	Save(ctx context.Context, uid string, tp TaskAndPriority) error
 }
 
-func New() *Hashira {
+type Hashira struct {
+	AccessTokenStore     AccessTokenStore
+	TaskAndPriorityStore TaskAndPriorityStore
+}
+
+func New(atStore AccessTokenStore, tpStore TaskAndPriorityStore) *Hashira {
 	return &Hashira{
-		AccessTokenStore: store.NewAccessTokenStore(),
+		AccessTokenStore:     atStore,
+		TaskAndPriorityStore: tpStore,
 	}
 }
 
 func (h *Hashira) TestAccessToken(w http.ResponseWriter, r *http.Request) {
-	if len(r.Header["Authorization"]) != 1 {
-		log.Println("Authorization header is missing or appears more than once. error.")
+	accesstoken, err := h.retrieveAccessTokenFromHeader(r.Context(), r.Header)
+	if err != nil {
+		log.Printf("failed to retrieve accesstoken from header: %v", err)
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 
-	authHeader := r.Header["Authorization"][0]
-	matches := bearerRegex.FindStringSubmatch(authHeader)
-	if len(matches) != 2 {
-		log.Printf("unexpected Authorization header format: %v", authHeader)
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	accesstoken := matches[1]
 	uid, err := h.AccessTokenStore.FindUidByAccessToken(r.Context(), accesstoken)
 	if err != nil {
-		log.Printf("could not find a user who has the accesstoken %v: %v", authHeader, err)
+		log.Printf("could not find a user who has the accesstoken %v: %v", accesstoken, err)
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
@@ -52,4 +51,65 @@ func (h *Hashira) TestAccessToken(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func (h *Hashira) retrieveAccessTokenFromHeader(ctx context.Context, header http.Header) (string, error) {
+	if len(header["Authorization"]) != 1 {
+		return "", errors.New("Authorization header is missing or appears more than once. error")
+	}
+
+	authHeader := header["Authorization"][0]
+	matches := bearerRegex.FindStringSubmatch(authHeader)
+	if len(matches) != 2 {
+		return "", fmt.Errorf("unexpected Authorization header format: %v", authHeader)
+	}
+
+	accesstoken := matches[1]
+	return accesstoken, nil
+}
+
+type Task struct {
+	ID        string
+	Name      string
+	Place     string
+	IsDeleted bool
+}
+
+type Priority map[string][]string
+
+// priority's key should be one of following strings:
+// "BACKLOG", "TODO", "DOING", "DONE"
+type TaskAndPriority struct {
+	Tasks    map[string]Task `json:"tasks"`
+	Priority Priority        `json:"priority"`
+}
+
+func (h *Hashira) Upload(w http.ResponseWriter, r *http.Request) {
+	accesstoken, err := h.retrieveAccessTokenFromHeader(r.Context(), r.Header)
+	if err != nil {
+		log.Printf("failed to retrieve accesstoken from header: %v", err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	uid, err := h.AccessTokenStore.FindUidByAccessToken(r.Context(), accesstoken)
+	if err != nil {
+		log.Printf("could not find a user who has the accesstoken %v: %v", accesstoken, err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	var tp TaskAndPriority
+	err = json.NewDecoder(r.Body).Decode(&tp)
+	if err != nil {
+		log.Printf("failed to read body: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := h.TaskAndPriorityStore.Save(r.Context(), uid, tp); err != nil {
+		log.Printf("failed to save tasks and priorities: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
