5b9189f86bd3ac3a505b3a4e33c0efc162d37efe
pankona <yosuke.akatsuka@gmail.com>
2021-10-11 22:54:34 +0900

    support delete accesstoken via hashira-web

--- hashira-web/src/App.tsx
+++ hashira-web/src/App.tsx
@@ -4,6 +4,9 @@ import * as firebase from "./firebase";
 const App: React.VFC = () => {
   const [user, setUser] = React.useState<firebase.User | null>(null);
   const [accesstokens, setAccessTokens] = React.useState<string[]>([]);
+  const [checkedTokens, setCheckedTokens] = React.useState<{
+    [key: string]: boolean;
+  }>({});
 
   React.useEffect(() => {
     firebase.onAuthStateChanged((user: firebase.User | null) => {
@@ -25,18 +28,57 @@ const App: React.VFC = () => {
         <>
           <div>{`Hello, ${user?.displayName!}!`}</div>
           <button
-            onClick={() => {
-              firebase.claimNewAccessToken(user.uid);
-              (async () => {
-                const ret = await firebase.fetchAccessTokens(user.uid);
-                setAccessTokens(ret);
-              })();
+            onClick={async () => {
+              await firebase.claimNewAccessToken(user.uid);
+
+              const ret = await firebase.fetchAccessTokens(user.uid);
+              setAccessTokens(ret);
             }}
           >
             Generate new access token
           </button>
+          <button
+            disabled={((): boolean => {
+              for (const v in checkedTokens) {
+                if (checkedTokens[v]) {
+                  return false;
+                }
+              }
+              return true;
+            })()}
+            onClick={async () => {
+              const accesstokens: string[] = [];
+              for (let v in checkedTokens) {
+                accesstokens.push(v);
+              }
+
+              await firebase.RevokeAccessTokens(user.uid, accesstokens);
+
+              const ret = await firebase.fetchAccessTokens(user.uid);
+              setAccessTokens(ret);
+            }}
+          >
+            Revoke access token
+          </button>
           {accesstokens.map((token: string) => {
-            return <li key={token}>{token}</li>;
+            return (
+              <li key={token}>
+                <input
+                  id={token}
+                  type="checkbox"
+                  checked={checkedTokens[token] || false}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setCheckedTokens({
+                      ...checkedTokens,
+                      [e.target.id]: e.target.checked,
+                    });
+                  }}
+                  name={token}
+                  value={token}
+                />
+                {token}
+              </li>
+            );
           })}
         </>
       ) : undefined}
