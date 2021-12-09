d79523a7c5660e0d9d1de11b67541c7bf7fac5b4
pankona <yosuke.akatsuka@gmail.com>
2021-12-09 22:02:45 +0900

    refactoring

--- hashira-web/src/App.tsx
+++ hashira-web/src/App.tsx
@@ -139,173 +139,186 @@ const App: React.VFC = () => {
 
   return (
     <div>
-      <Header user={user} />
+      <Header
+        user={user}
+        isLoading={!user || !accesstokens || !tasksAndPriorities}
+      />
       <StyledBody>
-        <TaskInput
-          onSubmitTasks={onSubmitTasks}
-          disabled={isLoading || !user}
-        />
+        {user !== null && (
+          <TaskInput
+            onSubmitTasks={onSubmitTasks}
+            disabled={isLoading || !user}
+          />
+        )}
         <StyledVerticalSpacer />
-        {user && accesstokens && tasksAndPriorities ? (
-          <>
-            <div style={{ display: "flex" }}>
-              <input
-                type="button"
-                value={"Mark as Done"}
-                style={{ minWidth: "128px" }}
-                disabled={
-                  isLoading ||
-                  ((): boolean => {
+        {(() => {
+          if (user === null) {
+            return <></>;
+          }
+
+          if (!user || !accesstokens || !tasksAndPriorities) {
+            return <div>Loading...</div>;
+          }
+
+          return (
+            <>
+              <div style={{ display: "flex" }}>
+                <input
+                  type="button"
+                  value={"Mark as Done"}
+                  style={{ minWidth: "128px" }}
+                  disabled={
+                    isLoading ||
+                    ((): boolean => {
+                      for (const v in checkedTasks) {
+                        if (checkedTasks[v]) {
+                          return false;
+                        }
+                      }
+                      return true;
+                    })()
+                  }
+                  onClick={async (e: React.FormEvent<HTMLInputElement>) => {
+                    e.preventDefault();
+                    if (!user) {
+                      return;
+                    }
+
+                    const tasksToMarkAsDone: firebase.TasksObject = {};
                     for (const v in checkedTasks) {
                       if (checkedTasks[v]) {
-                        return false;
+                        const task = tasksAndPriorities["Tasks"][v];
+                        tasksToMarkAsDone[v] = {
+                          ID: task.ID,
+                          IsDeleted: task.Place === "DONE",
+                          Name: task.Name,
+                          Place: "DONE",
+                        };
                       }
                     }
-                    return true;
-                  })()
-                }
-                onClick={async (e: React.FormEvent<HTMLInputElement>) => {
-                  e.preventDefault();
-                  if (!user) {
-                    return;
-                  }
 
-                  const tasksToMarkAsDone: firebase.TasksObject = {};
-                  for (const v in checkedTasks) {
-                    if (checkedTasks[v]) {
-                      const task = tasksAndPriorities["Tasks"][v];
-                      tasksToMarkAsDone[v] = {
-                        ID: task.ID,
-                        IsDeleted: task.Place === "DONE",
-                        Name: task.Name,
-                        Place: "DONE",
-                      };
-                    }
-                  }
-
-                  await updateTasks(tasksToMarkAsDone);
-                  // refresh tasks and priorities
-                  await fetchTasksAndPriorities(user.uid);
-                  setCheckedTasks({});
-                }}
-              />
-              <StyledHorizontalSpacer />
-              <input
-                type="button"
-                value={mode === "move" ? "Finish moving" : "Move"}
-                style={{ minWidth: "128px" }}
-                disabled={isLoading}
-                onClick={() => {
-                  setMode((prev) => {
-                    if (prev === "move") {
-                      return "select";
-                    }
+                    await updateTasks(tasksToMarkAsDone);
+                    // refresh tasks and priorities
+                    await fetchTasksAndPriorities(user.uid);
                     setCheckedTasks({});
-                    return "move";
-                  });
-                }}
-              />
-            </div>
-            <StyledVerticalSpacer />
-            {tasksAndPriorities ? (
-              <div
-                className="TaskAndPriorities"
-                style={{
-                  display: "flex",
-                  overflow: "auto",
-                }}
-              >
-                <TaskList
-                  place={"BACKLOG"}
-                  tasksAndPriorities={tasksAndPriorities}
-                  checkedTasks={checkedTasks}
-                  setCheckedTasks={setCheckedTasks}
-                  onEditTasks={onEditTasks}
-                  mode={mode}
-                  onMoveTask={onMoveTask}
+                  }}
                 />
-                <TaskList
-                  place={"TODO"}
-                  tasksAndPriorities={tasksAndPriorities}
-                  checkedTasks={checkedTasks}
-                  setCheckedTasks={setCheckedTasks}
-                  onEditTasks={onEditTasks}
-                  mode={mode}
-                  onMoveTask={onMoveTask}
-                />
-                <TaskList
-                  place={"DOING"}
-                  tasksAndPriorities={tasksAndPriorities}
-                  checkedTasks={checkedTasks}
-                  setCheckedTasks={setCheckedTasks}
-                  onEditTasks={onEditTasks}
-                  mode={mode}
-                  onMoveTask={onMoveTask}
-                />
-                <TaskList
-                  place={"DONE"}
-                  tasksAndPriorities={tasksAndPriorities}
-                  checkedTasks={checkedTasks}
-                  setCheckedTasks={setCheckedTasks}
-                  onEditTasks={onEditTasks}
-                  mode={mode}
-                  onMoveTask={onMoveTask}
+                <StyledHorizontalSpacer />
+                <input
+                  type="button"
+                  value={mode === "move" ? "Finish moving" : "Move"}
+                  style={{ minWidth: "128px" }}
+                  disabled={isLoading}
+                  onClick={() => {
+                    setMode((prev) => {
+                      if (prev === "move") {
+                        return "select";
+                      }
+                      setCheckedTasks({});
+                      return "move";
+                    });
+                  }}
                 />
               </div>
-            ) : undefined}
-            <button
-              onClick={async () => {
-                await firebase.claimNewAccessToken(user.uid);
-                await fetchAccessTokens(user.uid);
-              }}
-            >
-              Generate new access token
-            </button>
-            <button
-              disabled={((): boolean => {
-                for (const v in checkedTokens) {
-                  if (checkedTokens[v]) {
-                    return false;
-                  }
-                }
-                return true;
-              })()}
-              onClick={async () => {
-                const accesstokens: string[] = [];
-                for (let v in checkedTokens) {
-                  accesstokens.push(v);
-                }
-
-                await firebase.revokeAccessTokens(user.uid, accesstokens);
-                await fetchAccessTokens(user.uid);
-              }}
-            >
-              Revoke access token
-            </button>
-            {accesstokens.map((token: string) => {
-              return (
-                <li key={token}>
-                  <input
-                    id={token}
-                    type="checkbox"
-                    checked={checkedTokens[token] || false}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                      setCheckedTokens({
-                        ...checkedTokens,
-                        [e.target.id]: e.target.checked,
-                      });
-                    }}
-                    name={token}
-                    value={token}
+              <StyledVerticalSpacer />
+              {tasksAndPriorities ? (
+                <div
+                  className="TaskAndPriorities"
+                  style={{
+                    display: "flex",
+                    overflow: "auto",
+                  }}
+                >
+                  <TaskList
+                    place={"BACKLOG"}
+                    tasksAndPriorities={tasksAndPriorities}
+                    checkedTasks={checkedTasks}
+                    setCheckedTasks={setCheckedTasks}
+                    onEditTasks={onEditTasks}
+                    mode={mode}
+                    onMoveTask={onMoveTask}
                   />
-                  {token}
-                </li>
-              );
-            })}
-          </>
-        ) : (
-          <div>Loading...</div>
-        )}
+                  <TaskList
+                    place={"TODO"}
+                    tasksAndPriorities={tasksAndPriorities}
+                    checkedTasks={checkedTasks}
+                    setCheckedTasks={setCheckedTasks}
+                    onEditTasks={onEditTasks}
+                    mode={mode}
+                    onMoveTask={onMoveTask}
+                  />
+                  <TaskList
+                    place={"DOING"}
+                    tasksAndPriorities={tasksAndPriorities}
+                    checkedTasks={checkedTasks}
+                    setCheckedTasks={setCheckedTasks}
+                    onEditTasks={onEditTasks}
+                    mode={mode}
+                    onMoveTask={onMoveTask}
+                  />
+                  <TaskList
+                    place={"DONE"}
+                    tasksAndPriorities={tasksAndPriorities}
+                    checkedTasks={checkedTasks}
+                    setCheckedTasks={setCheckedTasks}
+                    onEditTasks={onEditTasks}
+                    mode={mode}
+                    onMoveTask={onMoveTask}
+                  />
+                </div>
+              ) : undefined}
+              <button
+                onClick={async () => {
+                  await firebase.claimNewAccessToken(user.uid);
+                  await fetchAccessTokens(user.uid);
+                }}
+              >
+                Generate new access token
+              </button>
+              <button
+                disabled={((): boolean => {
+                  for (const v in checkedTokens) {
+                    if (checkedTokens[v]) {
+                      return false;
+                    }
+                  }
+                  return true;
+                })()}
+                onClick={async () => {
+                  const accesstokens: string[] = [];
+                  for (let v in checkedTokens) {
+                    accesstokens.push(v);
+                  }
+
+                  await firebase.revokeAccessTokens(user.uid, accesstokens);
+                  await fetchAccessTokens(user.uid);
+                }}
+              >
+                Revoke access token
+              </button>
+              {accesstokens.map((token: string) => {
+                return (
+                  <li key={token}>
+                    <input
+                      id={token}
+                      type="checkbox"
+                      checked={checkedTokens[token] || false}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setCheckedTokens({
+                          ...checkedTokens,
+                          [e.target.id]: e.target.checked,
+                        });
+                      }}
+                      name={token}
+                      value={token}
+                    />
+                    {token}
+                  </li>
+                );
+              })}
+            </>
+          );
+        })()}
       </StyledBody>
     </div>
   );
