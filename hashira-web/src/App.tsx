09e1c826e7f0d716c827871749678be1b0040969
pankona <yosuke.akatsuka@gmail.com>
2021-12-09 17:40:58 +0900

    implement onMove

--- hashira-web/src/App.tsx
+++ hashira-web/src/App.tsx
@@ -38,6 +38,46 @@ const App: React.VFC = () => {
     setIsUploading(false);
   };
 
+  const onMoveTask = async (taskId: string, direction: "left" | "right") => {
+    if (!user) {
+      return;
+    }
+    const tasksToMove: firebase.TasksObject = {};
+    const task = tasksAndPriorities["Tasks"][taskId];
+    const currentIndex = firebase.Place.indexOf(task.Place);
+    const nextIndex = ((): number => {
+      if (direction === "left") {
+        if (currentIndex === 0) {
+          return firebase.Place.length - 1;
+        }
+        return currentIndex - 1;
+      }
+
+      if (currentIndex === firebase.Place.length - 1) {
+        return 0;
+      }
+      return currentIndex + 1;
+    })();
+
+    tasksToMove[taskId] = {
+      ID: task.ID,
+      IsDeleted: false,
+      Name: task.Name,
+      Place: firebase.Place[nextIndex],
+    };
+
+    setIsUploading(true);
+
+    await firebase.updateTasks(tasksToMove);
+
+    // refresh tasks and priorities
+    const tp = await firebase.fetchTaskAndPriorities(user.uid);
+    setTasksAndPriorities(tp);
+    setCheckedTasks({});
+
+    setIsUploading(false);
+  };
+
   React.useEffect(() => {
     firebase.onAuthStateChanged((user: firebase.User | null) => {
       if (!user) {
@@ -141,6 +181,7 @@ const App: React.VFC = () => {
                 setCheckedTasks={setCheckedTasks}
                 setTasksAndPriorities={setTasksAndPriorities}
                 mode={mode}
+                onMoveTask={onMoveTask}
               />
               <TaskList
                 user={user}
@@ -150,6 +191,7 @@ const App: React.VFC = () => {
                 setCheckedTasks={setCheckedTasks}
                 setTasksAndPriorities={setTasksAndPriorities}
                 mode={mode}
+                onMoveTask={onMoveTask}
               />
               <TaskList
                 user={user}
@@ -159,6 +201,7 @@ const App: React.VFC = () => {
                 setCheckedTasks={setCheckedTasks}
                 setTasksAndPriorities={setTasksAndPriorities}
                 mode={mode}
+                onMoveTask={onMoveTask}
               />
               <TaskList
                 user={user}
@@ -168,6 +211,7 @@ const App: React.VFC = () => {
                 setCheckedTasks={setCheckedTasks}
                 setTasksAndPriorities={setTasksAndPriorities}
                 mode={mode}
+                onMoveTask={onMoveTask}
               />
             </div>
           ) : undefined}
