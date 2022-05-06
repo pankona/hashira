8fd588d44db0ec3083211c0fdafcbe9ff769f5fb
pankona <yosuke.akatsuka@gmail.com>
2022-05-06 12:18:05 +0900

    add manifests and icons for supporting pwa

--- hashira-web/src/index.tsx
+++ hashira-web/src/index.tsx
@@ -11,3 +11,9 @@ const Root = () => (
 );
 
 ReactDOM.render(<Root />, document.getElementById("app"));
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/serviceworker.js").then(function () {
+    console.log("Service Worker is registered!!");
+  });
+}
