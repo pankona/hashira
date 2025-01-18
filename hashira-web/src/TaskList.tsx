6b9c3b5b71f93cd2f799d839d654502c149b84d2
Yosuke Akatsuka <yosuke.akatsuka@gmail.com>
2025-01-18 16:10:29 +0000

    Update React, React-DOM, and @types/react to latest versions and fix type errors

--- hashira-web/src/TaskList.tsx
+++ hashira-web/src/TaskList.tsx
@@ -51,7 +51,7 @@ const StyledArrow = styled.div`
   font-size: 12px;
 `;
 
-export const TaskList: React.VFC<{
+export const TaskList: React.FC<{
   place: typeof firebase.Places[number];
   tasksAndPriorities: firebase.TasksAndPriorities;
   checkedTasks: { [key: string]: boolean };
@@ -68,128 +68,128 @@ export const TaskList: React.VFC<{
   mode,
   onMoveTask,
 }) => {
-  const [updatedTasks, setUpdatedTasks] = React.useState<{
-    [key: string]: string;
-  }>({});
+    const [updatedTasks, setUpdatedTasks] = React.useState<{
+      [key: string]: string;
+    }>({});
 
-  // onEditCompleted は、どれかひとつの task の変更が終わると呼び出される (onBlur)。
-  //
-  // validation の後、string を firebase.TasksObject の形に変換して onEditTasks を呼び出す。
-  //
-  // 典型的な使い方をすると一度に更新される task はたかだかひとつであるが、onEditTasks 内で
-  // 通信エラーが起こるなどすると、変更が DB に反映されていない task が取り残される可能性がありそう。
-  // そのため、変更候補のものはすべていっぺんに更新するような処理にしている。
-  const onEditCompleted = async () => {
-    const tasksToUpdate: firebase.TasksObject = {};
-    for (const v in updatedTasks) {
-      if (updatedTasks[v] === "") {
-        delete updatedTasks[v];
-        setUpdatedTasks({
-          ...updatedTasks,
-        });
-        return;
+    // onEditCompleted は、どれかひとつの task の変更が終わると呼び出される (onBlur)。
+    //
+    // validation の後、string を firebase.TasksObject の形に変換して onEditTasks を呼び出す。
+    //
+    // 典型的な使い方をすると一度に更新される task はたかだかひとつであるが、onEditTasks 内で
+    // 通信エラーが起こるなどすると、変更が DB に反映されていない task が取り残される可能性がありそう。
+    // そのため、変更候補のものはすべていっぺんに更新するような処理にしている。
+    const onEditCompleted = async () => {
+      const tasksToUpdate: firebase.TasksObject = {};
+      for (const v in updatedTasks) {
+        if (updatedTasks[v] === "") {
+          delete updatedTasks[v];
+          setUpdatedTasks({
+            ...updatedTasks,
+          });
+          return;
+        }
+
+        const task = tasksAndPriorities["Tasks"][v];
+        tasksToUpdate[v] = {
+          ID: task.ID,
+          IsDeleted: false,
+          Name: updatedTasks[v],
+          Place: task.Place,
+        };
       }
 
-      const task = tasksAndPriorities["Tasks"][v];
-      tasksToUpdate[v] = {
-        ID: task.ID,
-        IsDeleted: false,
-        Name: updatedTasks[v],
-        Place: task.Place,
-      };
-    }
+      await onEditTasks(tasksToUpdate);
+      setUpdatedTasks({});
+    };
 
-    await onEditTasks(tasksToUpdate);
-    setUpdatedTasks({});
-  };
+    const noItem = [
+      <StyledListItem key="noitem" style={{ opacity: "0.3" }}>
+        No item
+      </StyledListItem>,
+    ];
 
-  const noItem = [
-    <StyledListItem key="noitem" style={{ opacity: "0.3" }}>
-      No item
-    </StyledListItem>,
-  ];
-
-  const convertTasksAndPrioritiesToJSXElement = (
-    tasksAndPriorities: firebase.TasksAndPriorities,
-  ): JSX.Element[] => {
-    if (!tasksAndPriorities["Priority"][place]) {
-      return noItem;
-    }
-    const filteredItems = tasksAndPriorities["Priority"][place].filter(
-      (v: string) => tasksAndPriorities["Tasks"][v],
-    );
-    if (filteredItems.length === 0) {
-      return noItem;
-    }
-
-    return filteredItems.map((p: string) => {
-      const taskId = tasksAndPriorities["Tasks"][p].ID;
-      const taskName = tasksAndPriorities["Tasks"][p].Name;
-      return (
-        <StyledListItem key={taskId} data-tasktoken={taskId}>
-          <>
-            <StyledListContent
-              style={{
-                marginRight: mode === "select" ? "0px" : "24px",
-              }}
-              id={`listContent-${taskId}`}
-              key={taskId}
-              value={updatedTasks[taskId] !== undefined
-                ? updatedTasks[taskId]
-                : taskName}
-              onChange={(e) => {
-                setUpdatedTasks({
-                  ...updatedTasks,
-                  [taskId]: e.target.value,
-                });
-              }}
-              onBlur={() => onEditCompleted()}
-            />
-
-            {mode === "select"
-              ? (
-                <StyledCheckbox
-                  id={`check-${taskId}`}
-                  value={taskName}
-                  onChange={(e) => {
-                    setCheckedTasks({
-                      ...checkedTasks,
-                      [taskId]: e.target.checked,
-                    });
-                  }}
-                />
-              )
-              : (
-                <StyledArrow id={`arrow-${taskId}`}>
-                  <div
-                    style={{ cursor: "pointer" }}
-                    onClick={(e) => onMoveTask(taskId, "left")}
-                  >
-                    👈
-                  </div>
-                  <div
-                    style={{ cursor: "pointer" }}
-                    onClick={(e) => onMoveTask(taskId, "right")}
-                  >
-                    👉
-                  </div>
-                </StyledArrow>
-              )}
-          </>
-        </StyledListItem>
+    const convertTasksAndPrioritiesToJSXElement = (
+      tasksAndPriorities: firebase.TasksAndPriorities,
+    ): React.ReactElement[] => {
+      if (!tasksAndPriorities["Priority"][place]) {
+        return noItem;
+      }
+      const filteredItems = tasksAndPriorities["Priority"][place].filter(
+        (v: string) => tasksAndPriorities["Tasks"][v],
       );
-    });
-  };
+      if (filteredItems.length === 0) {
+        return noItem;
+      }
 
-  return (
-    <StyledList>
-      <div
-        className="PlaceTitle"
-        style={{ minHeight: "24px", display: "flex", alignItems: "center" }}
-      >
-        {place}
-      </div>
-      {convertTasksAndPrioritiesToJSXElement(tasksAndPriorities)}
-    </StyledList>
-  );
-};
+      return filteredItems.map((p: string) => {
+        const taskId = tasksAndPriorities["Tasks"][p].ID;
+        const taskName = tasksAndPriorities["Tasks"][p].Name;
+        return (
+          <StyledListItem key={taskId} data-tasktoken={taskId}>
+            <>
+              <StyledListContent
+                style={{
+                  marginRight: mode === "select" ? "0px" : "24px",
+                }}
+                id={`listContent-${taskId}`}
+                key={taskId}
+                value={updatedTasks[taskId] !== undefined
+                  ? updatedTasks[taskId]
+                  : taskName}
+                onChange={(e) => {
+                  setUpdatedTasks({
+                    ...updatedTasks,
+                    [taskId]: e.target.value,
+                  });
+                }}
+                onBlur={() => onEditCompleted()}
+              />
+
+              {mode === "select"
+                ? (
+                  <StyledCheckbox
+                    id={`check-${taskId}`}
+                    value={taskName}
+                    onChange={(e) => {
+                      setCheckedTasks({
+                        ...checkedTasks,
+                        [taskId]: e.target.checked,
+                      });
+                    }}
+                  />
+                )
+                : (
+                  <StyledArrow id={`arrow-${taskId}`}>
+                    <div
+                      style={{ cursor: "pointer" }}
+                      onClick={(e) => onMoveTask(taskId, "left")}
+                    >
+                      👈
+                    </div>
+                    <div
+                      style={{ cursor: "pointer" }}
+                      onClick={(e) => onMoveTask(taskId, "right")}
+                    >
+                      👉
+                    </div>
+                  </StyledArrow>
+                )}
+            </>
+          </StyledListItem>
+        );
+      });
+    };
+
+    return (
+      <StyledList>
+        <div
+          className="PlaceTitle"
+          style={{ minHeight: "24px", display: "flex", alignItems: "center" }}
+        >
+          {place}
+        </div>
+        {convertTasksAndPrioritiesToJSXElement(tasksAndPriorities)}
+      </StyledList>
+    );
+  };
